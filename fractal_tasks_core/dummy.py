8506070c6ff0e8ea36366befd5b97d28a9de5789
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-10-05 10:52:14 +0200

    Add some headers

--- fractal_tasks_core/dummy.py
+++ fractal_tasks_core/dummy.py
@@ -1,3 +1,17 @@
+"""
+Copyright 2022 (C) Friedrich Miescher Institute for Biomedical Research and
+University of Zurich
+
+Original authors:
+Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
+Tommaso Comparin <tommaso.comparin@exact-lab.it>
+Marco Franzon <marco.franzon@exact-lab.it>
+
+This file is part of Fractal and was originally developed by eXact lab S.r.l.
+<exact-lab.it> under contract with Liberali Lab from the Friedrich Miescher
+Institute for Biomedical Research and Pelkmans Lab from the University of
+Zurich.
+"""
 import logging
 import os
 from pathlib import Path
