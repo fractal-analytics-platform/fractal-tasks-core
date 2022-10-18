81e9c1bb7bc1b4b8ce91f431412279a9897ecea5
mfranzon <franzonmarco94@gmail.com>
2022-10-18 19:41:16 +0200

    tries

--- fractal_tasks_core/cellpose_segmentation.py
+++ fractal_tasks_core/cellpose_segmentation.py
@@ -1,12 +1,6 @@
-# Copyright 2022 (C) Friedrich Miescher Institute for Biomedical Research and
-# University of Zurich
-# Original authors:
-# Tommaso Comparin <tommaso.comparin@exact-lab.it>
-# Marco Franzon <marco.franzon@exact-lab.it>
-# This file is part of Fractal and was originally developed by eXact lab S.r.l.
-# <exact-lab.it> under contract with Liberali Lab from the Friedrich Miescher
-# Institute for Biomedical Research and Pelkmans Lab from the University of
-# Zurich.
+"""
+Cellpose for segmentation task
+"""
 import json
 import logging
 import time
@@ -412,3 +406,15 @@ if __name__ == "__main__":
         labeling_channel=args.labeling_channel,
         labeling_level=args.labeling_level,
     )
+
+"""
+Copyright 2022 (C) Friedrich Miescher Institute for Biomedical Research and
+University of Zurich
+Original authors:
+Tommaso Comparin <tommaso.comparin@exact-lab.it>
+Marco Franzon <marco.franzon@exact-lab.it>
+This file is part of Fractal and was originally developed by eXact lab S.r.l.
+<exact-lab.it> under contract with Liberali Lab from the Friedrich Miescher
+Institute for Biomedical Research and Pelkmans Lab from the University of
+Zurich.
+"""
