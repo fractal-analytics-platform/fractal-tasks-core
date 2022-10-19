ecd7379193d46e4f17993c0922404c7982653866
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-10-19 15:10:37 +0200

    bump version 0.2.5 -> 0.2.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ copyright = (
     "2022, Friedrich Miescher Institute for Biomedical Research and "
     "University of Zurich"
 )
-version = "0.2.5"
+version = "0.2.6"
 language = "en"
 
 extensions = [
