8bd83d58ce1372554fa46030a52e8e53ff8cb9cc
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-10-19 15:09:21 +0200

    Update scripts/update_changelog.sh

--- scripts/update_changelog.sh
+++ scripts/update_changelog.sh
@@ -1,15 +1,3 @@
-
-CHANGELOG=CHANGELOG
-TMP=tmp_$CHANGELOG
-
-cat $CHANGELOG > $TMP
-
 LAST_TAG=`git tag --sort version:refname | tail -n 1`
-
-date > $CHANGELOG
 echo "Changes since $LAST_TAG:" >> $CHANGELOG
-git log --pretty="[%cs] %h - %s" ${PREVIOUS}..HEAD >> $CHANGELOG
-echo >> $CHANGELOG
-
-cat $TMP >> $CHANGELOG
-rm $TMP
+git log --pretty="[%cs] %h - %s" ${LAST_TAG}..HEAD
