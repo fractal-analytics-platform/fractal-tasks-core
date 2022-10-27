0b4e34ca75966b3d1a5f4da8a15b204d6fa21ce3
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-10-27 15:36:17 +0200

    Add provisional v1 manifest and validation script

---
+++ fractal_tasks_core/TEMPORARY_validate_manifest.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import TypeVar
+
+from pydantic import BaseModel
+
+
+__all__ = ("TaskManifestV1", "ManifestV1")
+
+
+class _TaskManifestBase(BaseModel):
+    name: str
+    input_type: str
+    output_type: str
+    default_args: Dict[str, Any]
+    executable: Path
+    meta: Dict[str, Any]
+
+
+TaskManifestType = TypeVar("TaskManifestType", bound=_TaskManifestBase)
+
+
+class _ManifestBase(BaseModel):
+    manifest_version: str
+    task_list: List[TaskManifestType]  # type: ignore
+
+
+class TaskManifestV1(_TaskManifestBase):
+    pass
+
+
+class ManifestV1(_ManifestBase):
+    task_list: List[TaskManifestV1]
+
+
+with open("__FRACTAL_MANIFEST__.json", "r") as fin:
+    manifest_dict = json.load(fin)
+print(json.dumps(manifest_dict, indent=4))
+manifest = ManifestV1(manifest_dict)
