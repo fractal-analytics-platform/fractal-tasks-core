e17936b0053067b57fc523c819f863dd5524fa72
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-10-31 13:39:41 +0100

    Add test_valid_manifest.py

---
+++ tests/test_valid_manifest.py
@@ -0,0 +1,28 @@
+import json
+import sys
+from pathlib import Path
+
+import requests
+from devtools import debug
+
+import fractal_tasks_core
+
+
+def test_valid_manifest(tmp_path):
+    url = (
+        "https://raw.githubusercontent.com/fractal-analytics-platform/"
+        "fractal-server/main/fractal_server/app/schemas/manifest.py"
+    )
+    r = requests.get(url)
+    debug(tmp_path)
+    with (tmp_path / "fractal_manifest.py").open("wb") as fout:
+        fout.write(r.content)
+
+    sys.path.append(str(tmp_path))
+    from fractal_manifest import ManifestV1
+
+    module_dir = Path(fractal_tasks_core.__file__).parent
+    with (module_dir / "__FRACTAL_MANIFEST__.json").open("r") as fin:
+        manifest_dict = json.load(fin)
+    manifest = ManifestV1(**manifest_dict)
+    debug(manifest)
