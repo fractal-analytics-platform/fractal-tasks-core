d22c954a44db7a088d05b142cbfb890256ece40d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-04 12:10:26 +0100

    Port zenodo_zarr from 149_expected_dimensions_in_napari_workflows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import json
 import os
+import shutil
 from pathlib import Path
 from urllib.parse import unquote
 
@@ -15,7 +16,7 @@ def testdata_path() -> Path:
 
 
 @pytest.fixture(scope="session")
-def dataset_10_5281_zenodo_7059515(testdata_path):
+def zenodo_images(testdata_path):
     # Based on
     # https://github.com/dvolgyes/zenodo_get/blob/master/zenodo_get/zget.py
 
@@ -43,3 +44,63 @@ def dataset_10_5281_zenodo_7059515(testdata_path):
         wget.download(link, out=folder)
         print()
     return Path(folder)
+
+
+@pytest.fixture(scope="session")
+def zenodo_zarr(testdata_path, tmpdir_factory):
+
+    doi = "10.5281/zenodo.7274533"
+    rootfolder = testdata_path / (doi.replace(".", "_").replace("/", "_"))
+    platenames = ["plate.zarr", "plate_mip.zarr"]
+    folders = [rootfolder / plate for plate in platenames]
+
+    if rootfolder.exists():
+        print(f"{str(rootfolder)} already exists, skip")
+    else:
+        rootfolder.mkdir()
+        tmp_path = tmpdir_factory.mktemp("zenodo_zarr")
+        zarrnames = [
+            "20200812-CardiomyocyteDifferentiation14-Cycle1.zarr",
+            "20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr",
+        ]
+        for zarrname, folder in zip(zarrnames, folders):
+            zipname = f"{zarrname}.zip"
+            url = f"https://zenodo.org/record/7274533/files/{zipname}"
+            wget.download(url, out=str(tmp_path / zipname), bar=None)
+            shutil.unpack_archive(
+                str(tmp_path / zipname), extract_dir=rootfolder, format="zip"
+            )
+            shutil.move(str(rootfolder / zarrname), str(folder))
+
+    return folders
+
+
+@pytest.fixture(scope="session")
+def zenodo_zarr_metadata(testdata_path):
+    metadata_3D = {
+        "plate": ["plate.zarr"],
+        "well": ["plate.zarr/B/03/0/"],
+        "num_levels": 6,
+        "coarsening_xy": 2,
+        "channel_list": ["A01_C01"],
+        "original_paths": [
+            str(testdata_path / "10_5281_zenodo_7059515/*.png")
+        ],
+    }
+
+    metadata_2D = {
+        "plate": ["plate.zarr"],
+        "well": ["plate_mip.zarr/B/03/0/"],
+        "num_levels": 6,
+        "coarsening_xy": 2,
+        "channel_list": ["A01_C01"],
+        "original_paths": [
+            str(testdata_path / "10_5281_zenodo_7059515/*.png")
+        ],
+        "replicate_zarr": {
+            "suffix": "mip",
+            "sources": {"plate_mip": "/this/should/not/be/used/"},
+        },
+    }
+
+    return [metadata_3D, metadata_2D]
