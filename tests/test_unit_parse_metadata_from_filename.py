2f3e96ed6a094a9a10e16cd7d793f5b471dfb41e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-08 14:31:57 +0100

    Improve tests for filename parsing (ref #189)

--- tests/test_unit_parse_metadata_from_filename.py
+++ tests/test_unit_parse_metadata_from_filename.py
@@ -22,33 +22,43 @@ f1 = (
 f2 = "210305NAR005AAN_210416_164828_B11_T0001F006L01A04Z14C01.tif"
 f3 = "220304_172545_220304_175557_L06_T0277F004L277A04Z07C04.tif"
 f4 = "220517CS001XXXIl_220715_151525_D05_T0001F001L01A01Z01C04.tif"
+f5 = "AssayPlate_Greiner_#655090_B02_T0001F004L01A01Z01C01.tif"
+f6 = "AssayPlate_Greiner_#655090_B02_T0001F004L01A01Z000000001C01.tif"
 
 p1 = "20200812-CardiomyocyteDifferentiation14-Cycle1"
 p2 = "210305NAR005AAN"
 p3 = "RS220304172545"
 p4 = "220517CS001XXXIl"
+p5 = "AssayPlate_Greiner_#655090"
+p6 = "AssayPlate_Greiner_#655090"
 
 A1 = "01"
 A2 = "04"
 A3 = "04"
 A4 = "01"
+A5 = "01"
+A6 = "01"
 
 C1 = "01"
 C2 = "01"
 C3 = "04"
 C4 = "04"
+C5 = "01"
+C6 = "01"
 
 parameters = [
     (f1, p1, A1, C1),
     (f2, p2, A2, C2),
     (f3, p3, A3, C3),
     (f4, p4, A4, C4),
+    (f5, p5, A5, C5),
+    (f6, p6, A6, C6),
 ]
 
 
 @pytest.mark.parametrize("filename,plate,A,C", parameters)
 def test_parse_metadata_from_image_filename(filename, plate, A, C):
     metadata = parse_filename(filename)
-    # assert metadata["plate"] == plate
+    assert metadata["plate"] == plate
     assert metadata["A"] == A
     assert metadata["C"] == C
