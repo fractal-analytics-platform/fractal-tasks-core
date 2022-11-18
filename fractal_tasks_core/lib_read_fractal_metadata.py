50875a4872173a963716b2a2ecd85432f6823a93
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-18 08:57:00 +0100

    Fix discover_acquisition_from_ome_zarr function

--- fractal_tasks_core/lib_read_fractal_metadata.py
+++ fractal_tasks_core/lib_read_fractal_metadata.py
@@ -10,15 +10,24 @@ import zarr
 def discover_acquisition_from_ome_zarr(
     well_zarr_path: Path, component: str
 ) -> int:
+    """
+    FIXME: write docstring
+
+    : param well_zarr_path: TBD
+    : param component: TBD
+    """
     well_path = Path(component).parent
     image_path = Path(component).name
     well_group = zarr.open_group(str(well_zarr_path.parent / well_path))
-    acquisition = next(
-        item["acquisition"]
-        for item in well_group.attrs["well"]["images"]
-        if item["path"] == image_path
-    )
-    return acquisition
+    try:
+        acquisition = next(
+            item["acquisition"]
+            for item in well_group.attrs["well"]["images"]
+            if item["path"] == image_path
+        )
+        return acquisition
+    except KeyError:
+        return None
 
 
 def get_parameter_from_metadata(
@@ -27,8 +36,19 @@ def get_parameter_from_metadata(
     well_zarr_path: Path,
     component: str,
 ) -> List:
+
+    """
+    FIXME: write docstring
+
+    : param keys: TBD
+    : param metadata: TBD
+    : param well_zarr_path: TBD
+    : param component: TBD
+    """
+
     parameters = []
     acquisition = discover_acquisition_from_ome_zarr(well_zarr_path, component)
+
     for key in keys:
         try:
             parameter = metadata[key][acquisition]
