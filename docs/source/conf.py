e99fdb593696722ebe1c793c856010e197ec27bc
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-02-08 16:12:05 +0100

    bump version 0.7.1a0 -> 0.7.2a0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ copyright = (
     "2022, Friedrich Miescher Institute for Biomedical Research and "
     "University of Zurich"
 )
-version = "0.7.1a0"
+version = "0.7.2a0"
 language = "en"
 
 extensions = [
