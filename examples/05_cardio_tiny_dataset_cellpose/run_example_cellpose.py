7b181d58c6c9554ade65c167329dfb0fe6fe9334
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-02-15 14:48:09 +0100

    Update examples/05_cardio_tiny_dataset_cellpose (ref #276)

--- examples/05_cardio_tiny_dataset_cellpose/run_example_cellpose.py
+++ examples/05_cardio_tiny_dataset_cellpose/run_example_cellpose.py
@@ -17,30 +17,34 @@ from pathlib import Path
 from devtools import debug
 
 from fractal_tasks_core.cellpose_segmentation import cellpose_segmentation
-from fractal_tasks_core.create_zarr_structure import create_zarr_structure
-from fractal_tasks_core.yokogawa_to_zarr import yokogawa_to_zarr
+from fractal_tasks_core.create_ome_zarr import create_ome_zarr
+from fractal_tasks_core.yokogawa_to_ome_zarr import yokogawa_to_ome_zarr
 
 
-channel_parameters = {
-    "A01_C01": {
+allowed_channels = [
+    {
         "label": "DAPI",
+        "wavelength_id": "A01_C01",
         "colormap": "00FFFF",
         "start": 0,
         "end": 700,
     },
-    "A01_C02": {
+    {
+        "wavelength_id": "A01_C02",
         "label": "nanog",
         "colormap": "FF00FF",
         "start": 0,
         "end": 180,
     },
-    "A02_C03": {
+    {
+        "wavelength_id": "A02_C03",
         "label": "Lamin B1",
         "colormap": "FFFF00",
         "start": 0,
         "end": 1500,
     },
-}
+]
+
 
 num_levels = 6
 coarsening_xy = 2
@@ -57,10 +61,11 @@ zarr_path = Path("tmp_out/*.zarr")
 metadata = {}
 
 # Create zarr structure
-metadata_update = create_zarr_structure(
+metadata_update = create_ome_zarr(
     input_paths=[img_path],
     output_path=zarr_path,
-    channel_parameters=channel_parameters,
+    metadata=metadata,
+    allowed_channels=allowed_channels,
     num_levels=num_levels,
     coarsening_xy=coarsening_xy,
     metadata_table="mrf_mlf",
@@ -70,7 +75,7 @@ debug(metadata)
 
 # Yokogawa to zarr
 for component in metadata["image"]:
-    yokogawa_to_zarr(
+    yokogawa_to_ome_zarr(
         input_paths=[zarr_path],
         output_path=zarr_path,
         metadata=metadata,
@@ -85,8 +90,8 @@ for component in metadata["image"]:
         output_path=zarr_path,
         metadata=metadata,
         component=component,
-        labeling_channel="A01_C01",
-        labeling_level=0,
+        wavelength_id="A01_C01",
+        level=3,
         relabeling=True,
         diameter_level0=80.0,
     )
