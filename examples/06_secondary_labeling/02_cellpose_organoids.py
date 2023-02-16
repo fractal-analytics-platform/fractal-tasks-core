35bb6e2dfdbd13f7c9fdacdcece8d69c4e53b17b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-02-16 16:08:21 +0100

    First commit for examples/06_secondary_labeling

---
+++ examples/06_secondary_labeling/02_cellpose_organoids.py
@@ -0,0 +1,44 @@
+import json
+import os
+import shutil
+from pathlib import Path
+
+from devtools import debug
+
+from fractal_tasks_core.cellpose_segmentation import cellpose_segmentation
+
+
+if os.path.exists("tmp"):
+    shutil.rmtree("tmp")
+os.mkdir("tmp")
+shutil.copytree(
+    "output/RS220304172545_mip.zarr", "tmp/RS220304172545_mip.zarr"
+)
+
+# Init
+zarr_path_mip = Path("tmp/*zarr")
+with open("01_final_metadata.json", "r") as f:
+    metadata = json.load(f)
+
+
+# Cellpose for organoids
+for component in metadata["image"]:
+    cellpose_segmentation(
+        input_paths=[zarr_path_mip],
+        output_path=zarr_path_mip,
+        metadata=metadata,
+        component=component,
+        wavelength_id="A01_C01",
+        level=3,
+        relabeling=True,
+        diameter_level0=1000.0,
+        ROI_table_name="well_ROI_table",
+        cellprob_threshold=-3.0,
+        flow_threshold=0.4,
+        pretrained_model="model/Hummingbird.331986",
+        output_label_name="organoids",
+    )
+
+debug(metadata)
+with open("02_final_metadata.json", "w") as f:
+    json.dump(metadata, f, indent=4, sort_keys=True)
