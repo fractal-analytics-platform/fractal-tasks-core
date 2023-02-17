3f2ba78562fb9e8a9eb286fd9a0e8586e833ab96
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-02-17 12:36:55 +0100

    Update examples/06_secondary_labeling

--- examples/06_secondary_labeling/02_cellpose_two_steps_segmentation.py
+++ examples/06_secondary_labeling/02_cellpose_two_steps_segmentation.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from devtools import debug
 
 from fractal_tasks_core.cellpose_segmentation import cellpose_segmentation
+from prototype_secondary_labeling import cellpose_segmentation_bis
 
 
 if os.path.exists("tmp"):
@@ -19,6 +20,7 @@ shutil.copytree(
 zarr_path_mip = Path("tmp/*zarr")
 with open("01_final_metadata.json", "r") as f:
     metadata = json.load(f)
+debug(metadata)
 
 
 # Cellpose for organoids
@@ -36,9 +38,26 @@ for component in metadata["image"]:
         cellprob_threshold=-3.0,
         flow_threshold=0.4,
         pretrained_model="model/Hummingbird.331986",
-        output_label_name="organoids",
+        output_label_name="organoids_ROI_table",
+        bounding_box_ROI_table_name="organoids_bbox_table",
     )
 
-debug(metadata)
-with open("02_final_metadata.json", "w") as f:
-    json.dump(metadata, f, indent=4, sort_keys=True)
+print("\n--------------------\n")
+
+for component in metadata["image"]:
+    cellpose_segmentation_bis(
+        input_paths=[zarr_path_mip],
+        output_path=zarr_path_mip,
+        metadata=metadata,
+        component=component,
+        wavelength_id="A01_C01",
+        level=3,
+        relabeling=True,
+        diameter_level0=70.0,
+        ROI_table_name="organoids_bbox_table",
+        primary_label_ROI_table_name="organoids_ROI_table",
+        output_label_name="nuclei_ROI_table",
+        model_type="nuclei",
+        # cellprob_threshold=-3.0,
+        flow_threshold=0.4,
+        )
