2fe4c78f7dbe414425d2342b9073016973d3fdc1
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-03-02 15:35:19 +0100

    Clean up lib_glob.py

--- fractal_tasks_core/lib_glob.py
+++ fractal_tasks_core/lib_glob.py
@@ -13,23 +13,28 @@ def glob_with_multiple_patterns(
     patterns: Sequence[str] = None,
 ) -> set[str]:
     """
-    List all files and folders in a folder that simultaneously match a series
-    of glob patterns
+    List all the items (files and folders) in a given folder that
+    simultaneously match a series of glob patterns
 
     Arguments:
-        :folder: TBD
-        :patterns: TBD
+        :folder: Base folder where items will be searched.
+        :patterns: If specified, the list of patterns (defined as in
+                   https://docs.python.org/3/library/fnmatch.html) that item
+                   names will match with.
     """
 
+    # Sanitize base-folder path
     if folder.endswith("/"):
         actual_folder = folder[:-1]
     else:
         actual_folder = folder[:]
 
+    # If not pattern is specified, look for *all* items in the base folder
     if not patterns:
         patterns = ["*"]
-    logging.info(f"[glob_with_multiple_patterns] {patterns=}")
 
+    # Combine multiple glob searches (via set intersection)
+    logging.info(f"[glob_with_multiple_patterns] {patterns=}")
     items = None
     for pattern in patterns:
         new_matches = glob(f"{actual_folder}/{pattern}")
@@ -38,6 +43,6 @@ def glob_with_multiple_patterns(
         else:
             items = items.intersection(new_matches)
     items = items or set()
-
     logging.info(f"[glob_with_multiple_patterns] Found {len(items)} items")
+
     return items
