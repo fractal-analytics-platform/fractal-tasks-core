414d7401376e12806d3cb56cbd99d83a333a1cf7
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-22 15:40:45 +0200

    Add test_comparison_with_function_arguments

--- tests/test_valid_args_schemas.py
+++ tests/test_valid_args_schemas.py
@@ -2,20 +2,22 @@ import json
 from importlib import import_module
 from pathlib import Path
 
+from devtools import debug
+
 import fractal_tasks_core
 
 
 FRACTAL_TASKS_CORE_DIR = Path(fractal_tasks_core.__file__).parent
 
 
-def get_task_list_from_manifest() -> list[dict]:
+def _get_task_list_from_manifest() -> list[dict]:
     with (FRACTAL_TASKS_CORE_DIR / "__FRACTAL_MANIFEST__.json").open("r") as f:
         manifest = json.load(f)
     task_list = manifest["task_list"]
     return task_list
 
 
-def create_schema_for_single_task(task: dict):
+def _create_schema_for_single_task(task: dict):
     executable = task["executable"]
     if not executable.endswith(".py"):
         raise ValueError(f"Invalid {executable=}")
@@ -26,18 +28,22 @@ def create_schema_for_single_task(task: dict):
     return schema, module_name
 
 
+def _extract_function(task: dict):
+    executable = task["executable"]
+    if not executable.endswith(".py"):
+        raise ValueError(f"Invalid {executable=}")
+    module_name = executable[:-3]
+    module = import_module(f"fractal_tasks_core.{module_name}")
+    task_function = getattr(module, module_name)
+    return task_function
+
+
 def test_task_arguments_schemas():
 
-    task_list = get_task_list_from_manifest()
+    task_list = _get_task_list_from_manifest()
     for task in task_list:
         print(f"Now handling {task['executable']}")
-        try:
-            schema, module_name = create_schema_for_single_task(task)
-        except AttributeError as e:
-            print(f"Skip {module_name}, due to AttributeError")
-            print()
-            raise e
-
+        schema, module_name = _create_schema_for_single_task(task)
         schema_path = FRACTAL_TASKS_CORE_DIR / f"__args__{module_name}__.json"
         with schema_path.open("r") as f:
             current_schema = json.load(f)
@@ -45,3 +51,19 @@ def test_task_arguments_schemas():
             raise ValueError("Schemas are different.")
         print(f"Schema in {schema_path.as_posix()} is up-to-date.")
         print()
+
+
+def test_comparison_with_function_arguments():
+    task_list = _get_task_list_from_manifest()
+    for task in task_list:
+        print(f"Now handling {task['executable']}")
+        schema, module_name = _create_schema_for_single_task(task)
+        fun = _extract_function(task)
+        debug(fun)
+        names_from_signature = set(
+            name
+            for name, _type in fun.__annotations__.items()
+            if name != "return"
+        )
+        name_from_args_schema = set(schema["properties"].keys())
+        assert names_from_signature == name_from_args_schema
