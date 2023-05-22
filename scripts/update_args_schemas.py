e0ab1133f28ede577f40534f22a6eeb362dfe93e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-22 15:24:20 +0200

    Update scripts/update_args_schemas.py and add it to github CI

--- scripts/update_args_schemas.py
+++ scripts/update_args_schemas.py
@@ -2,6 +2,7 @@
 This script generates JSON schemas for task arguments afresh, and writes them
 to files starting like `fractal_tasks_core/__args__create_ome_zarr__.json`
 """
+import argparse
 import json
 from importlib import import_module
 from pathlib import Path
@@ -9,27 +10,69 @@ from pathlib import Path
 import fractal_tasks_core
 
 
-# Load manifest
-module_dir = Path(fractal_tasks_core.__file__).parent
-with (module_dir / "__FRACTAL_MANIFEST__.json").open("r") as f:
-    manifest = json.load(f)
+FRACTAL_TASKS_CORE_DIR = Path(fractal_tasks_core.__file__).parent
 
-for task in manifest["task_list"]:
+
+def get_task_list_from_manifest() -> list[dict]:
+    with (FRACTAL_TASKS_CORE_DIR / "__FRACTAL_MANIFEST__.json").open("r") as f:
+        manifest = json.load(f)
+    task_list = manifest["task_list"]
+    return task_list
+
+
+def create_schema_for_single_task(task: dict):
     executable = task["executable"]
     if not executable.endswith(".py"):
         raise ValueError(f"Invalid {executable=}")
     module_name = executable[:-3]
-    print(f"Now handling {module_name}")
     module = import_module(f"fractal_tasks_core.{module_name}")
-    try:
-        TaskArguments = getattr(module, "TaskArguments")
-    except AttributeError:
-        print(f"Skip {module_name}, due to AttributeError")
-        print()
-        continue
+    TaskArguments = getattr(module, "TaskArguments")
     schema = TaskArguments.schema()
-    schema_path = module_dir / f"__args__{module_name}__.json"
-    with schema_path.open("w") as f:
-        json.dump(schema, f, indent=2)
-    print(f"Schema written to {schema_path.as_posix()}")
-    print()
+    return schema, module_name
+
+
+if __name__ == "__main__":
+
+    parser_main = argparse.ArgumentParser(
+        description="Create/update task-arguments JSON schemas"
+    )
+    subparsers_main = parser_main.add_subparsers(
+        title="Commands:", dest="command", required=True
+    )
+    parser_check = subparsers_main.add_parser(
+        "check",
+        description="Check that existing files are up-to-date",
+        allow_abbrev=False,
+    )
+    parser_check = subparsers_main.add_parser(
+        "new",
+        description="Write new JSON schemas to files",
+        allow_abbrev=False,
+    )
+
+    args = parser_main.parse_args()
+    command = args.command
+
+    task_list = get_task_list_from_manifest()
+    for task in task_list:
+        print(f"Now handling {task['executable']}")
+        try:
+            schema, module_name = create_schema_for_single_task(task)
+        except AttributeError:
+            print(f"Skip {module_name}, due to AttributeError")
+            print()
+            continue
+
+        schema_path = FRACTAL_TASKS_CORE_DIR / f"__args__{module_name}__.json"
+        if command == "check":
+            with schema_path.open("r") as f:
+                current_schema = json.load(f)
+            if not current_schema == schema:
+                raise ValueError("Schemas are different.")
+            print(f"Schema in {schema_path.as_posix()} is up-to-date.")
+            print()
+        elif command == "new":
+            with schema_path.open("w") as f:
+                json.dump(schema, f, indent=2)
+            print(f"Schema written to {schema_path.as_posix()}")
+            print()
