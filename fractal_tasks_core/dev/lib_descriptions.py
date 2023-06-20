1a8512b0b42582185223b91ab8da77e465889b85
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-06-20 11:07:22 +0200

    moved function and add docs

--- fractal_tasks_core/dev/lib_descriptions.py
+++ fractal_tasks_core/dev/lib_descriptions.py
@@ -5,49 +5,6 @@ from docstring_parser import parse as docparse
 
 import fractal_tasks_core
 
-inner_pydantic_models = {
-    "OmeroChannel": "lib_channels.py",
-    "Window": "lib_channels.py",
-    "Channel": "tasks/_input_models.py",
-    "NapariWorkflowsInput": "tasks/_input_models.py",
-    "NapariWorkflowsOutput": "tasks/_input_models.py",
-}
-
-
-def _get_args_model_descriptions():
-
-    descriptions = {}
-
-    for model, module in inner_pydantic_models.items():
-
-        module_path = Path(fractal_tasks_core.__file__).parent / module
-        tree = ast.parse(module_path.read_text())
-        _class = next(
-            c
-            for c in ast.walk(tree)
-            if (isinstance(c, ast.ClassDef) and c.name == model)
-        )
-        if not _class:
-            raise RuntimeError(f"Model {module_path}::{model} not found.")
-        else:
-            descriptions[model] = {}
-
-        docstring = ast.get_docstring(_class)
-        if docstring:
-            descriptions[model]["__docstring__"] = docstring
-
-        var_name: str = ""
-        for node in _class.body:
-            if isinstance(node, ast.AnnAssign):
-                descriptions[model][node.target.id] = None
-                var_name = node.target.id
-            else:
-                if isinstance(node, ast.Expr) and var_name:
-                    descriptions[model][var_name] = node.value.s
-                    var_name = ""
-
-    return descriptions
-
 
 def _get_args_descriptions(executable) -> dict[str, str]:
     """
@@ -91,3 +48,51 @@ def _include_args_descriptions_in_schema(*, schema, descriptions):
             new_properties[key] = value
     new_schema["properties"] = new_properties
     return new_schema
+
+
+INNER_PYDANTIC_MODELS = {
+    "OmeroChannel": "lib_channels.py",
+    "Window": "lib_channels.py",
+    "Channel": "tasks/_input_models.py",
+    "NapariWorkflowsInput": "tasks/_input_models.py",
+    "NapariWorkflowsOutput": "tasks/_input_models.py",
+}
+
+
+def _get_args_model_descriptions(
+    models: dict[str, str] = INNER_PYDANTIC_MODELS
+):
+    """
+    Extract attribut descriptions for Pydantic models
+    """
+    descriptions = {}
+
+    for model, module in models.items():
+        # get the class
+        module_path = Path(fractal_tasks_core.__file__).parent / module
+        tree = ast.parse(module_path.read_text())
+        _class = next(
+            c
+            for c in ast.walk(tree)
+            if (isinstance(c, ast.ClassDef) and c.name == model)
+        )
+        if not _class:
+            raise RuntimeError(f"Model {module_path}::{model} not found.")
+        else:
+            descriptions[model] = {}
+
+        # extract class docstring and attribute docstrings
+        docstring = ast.get_docstring(_class)
+        if docstring:
+            descriptions[model]["_class_docstring_"] = docstring
+        var_name: str = ""
+        for node in _class.body:
+            if isinstance(node, ast.AnnAssign):
+                descriptions[model][node.target.id] = None
+                var_name = node.target.id
+            else:
+                if isinstance(node, ast.Expr) and var_name:
+                    descriptions[model][var_name] = node.value.s
+                    var_name = ""
+
+    return descriptions
