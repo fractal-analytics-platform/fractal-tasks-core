26a30b670f418b8b84ee6605ceee495aefd45d48
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-22 14:28:34 +0200

    Update test_unit_lib_descriptions.py
    1. Remove `test_descriptions`, since it would always fail because descriptions are already in the manifest;
    2. Update other tests.

--- tests/dev/test_unit_lib_descriptions.py
+++ tests/dev/test_unit_lib_descriptions.py
@@ -1,5 +1,3 @@
-import json
-
 from devtools import debug
 
 from fractal_tasks_core.dev.lib_descriptions import (
@@ -8,13 +6,6 @@ from fractal_tasks_core.dev.lib_descriptions import (
 from fractal_tasks_core.dev.lib_descriptions import (
     _get_function_args_descriptions,
 )
-from fractal_tasks_core.dev.lib_descriptions import (
-    _insert_class_attrs_descriptions,
-)
-
-# from fractal_tasks_core.dev.lib_descriptions import (
-#     _insert_function_args_descriptions,
-# )
 
 
 def test_get_function_args_descriptions():
@@ -24,7 +15,9 @@ def test_get_function_args_descriptions():
         "_extract_function",
     )
     debug(args_descriptions)
-    assert args_descriptions.keys() == set(("executable", "package"))
+    assert args_descriptions.keys() == set(
+        ("package_name", "module_relative_path", "function_name")
+    )
 
 
 def test_get_class_attrs_descriptions():
@@ -33,27 +26,3 @@ def test_get_class_attrs_descriptions():
     )
     debug(attrs_descriptions)
     assert attrs_descriptions.keys() == set(("wavelength_id", "label"))
-
-
-def test_descriptions():
-    FILE = "lib_channels.py"
-    CLASS = "OmeroChannel"
-
-    descriptions = _get_class_attrs_descriptions(
-        "fractal_tasks_core", FILE, CLASS
-    )
-
-    with open("fractal_tasks_core/__FRACTAL_MANIFEST__.json", "r") as f:
-        manifest = json.load(f)
-
-    schemas = [task["args_schema"] for task in manifest["task_list"]]
-
-    for _, schema in enumerate(schemas):
-        new_schema = _insert_class_attrs_descriptions(
-            schema=schema, class_name=CLASS, descriptions=descriptions
-        )
-        if "definitions" in schema:
-            for class_name, definition in new_schema["definitions"].items():
-                if class_name == "OmeroChannel":
-                    for prop in definition["properties"]:
-                        assert "description" in definition["properties"][prop]
