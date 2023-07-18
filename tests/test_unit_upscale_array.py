bb85c71d4aba99d0c095aee203e58745a8bd094d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-18 12:18:12 +0200

    Extend test_unit_upscale_array.py to some specific failure modes

--- tests/test_unit_upscale_array.py
+++ tests/test_unit_upscale_array.py
@@ -1,6 +1,5 @@
-from typing import List
+from typing import Optional
 from typing import Sequence
-from typing import Tuple
 
 import numpy as np
 import pytest
@@ -10,7 +9,7 @@ from fractal_tasks_core.lib_upscale_array import convert_region_to_low_res
 from fractal_tasks_core.lib_upscale_array import upscale_array
 
 
-list_success: List[Tuple] = []
+list_success: list[tuple] = []
 list_success.append(((1, 2, 3), (1, 2, 3), [0, 1, 2]))
 list_success.append(((1, 2, 3), (1, 2, 3), [0, 1]))
 list_success.append(((1, 2, 3), (1, 2, 3), [0]))
@@ -21,7 +20,9 @@ list_success.append(((1, 2, 3), (1, 4, 12), [1, 2]))
 
 @pytest.mark.parametrize("old_shape,target_shape,axis", list_success)
 def test_upscale_array_success(
-    old_shape: Sequence[int], target_shape: Sequence[int], axis: Sequence[int]
+    old_shape: Sequence[int],
+    target_shape: tuple[int, ...],
+    axis: Optional[Sequence[int]],
 ):
     old_array = np.ones(old_shape)
     new_array = upscale_array(
@@ -30,20 +31,40 @@ def test_upscale_array_success(
     assert new_array.shape == target_shape
 
 
-list_fail: List[Tuple] = []
+list_fail: list[tuple] = []
 list_fail.append(((1, 2, 3, 4), (1, 2, 6), [0, 1]))
-list_fail.append(((1, 2, 3, 4), (1, 2, 6), [0, -2]))
+list_fail.append(((1, 2, 3, 4), (1, 2, 6), [0, 2]))
 list_fail.append(((1, 4), (1, 2), None))
 list_fail.append(((1, 4), (1, 2), [0]))
+list_fail.append(((4,), (2,), []))
+list_fail.append(((2, 2), (4, 4), [0]))
 
 
 @pytest.mark.parametrize("old_shape,target_shape,axis", list_fail)
 def test_upscale_array_fail(
-    old_shape: Sequence[int], target_shape: Sequence[int], axis: Sequence[int]
+    old_shape: Sequence[int],
+    target_shape: tuple[int, ...],
+    axis: Sequence[int],
 ):
     old_array = np.ones(old_shape)
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as e:
         upscale_array(target_shape=target_shape, array=old_array, axis=axis)
+    debug(e.value)
+
+
+def test_upscale_array_inhomogeneous_warning():
+    old_shape = (1, 1)
+    old_array = np.ones(old_shape)
+    with pytest.warns(UserWarning) as w:
+        upscale_array(
+            target_shape=(2, 3),
+            array=old_array,
+            axis=None,
+            warn_if_inhomogeneous=True,
+        )
+    warning_message = w.list[0].message
+    debug(warning_message)
+    assert "inhomogeneous" in str(warning_message)
 
 
 def test_incommensurable_upscaling():
@@ -88,15 +109,28 @@ def test_convert_region_to_low_res():
     assert lowres_region == expected_lowres_region
 
     # Conversion in the wrong direction
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as e:
         convert_region_to_low_res(
             highres_shape=lowres_shape,
             lowres_shape=highres_shape,
             highres_region=highres_region,
         )
+    debug(e.value)
+
+    # Dimension mismatch
+    with pytest.raises(ValueError) as e:
+        highres_shape = (8,)
+        lowres_shape = (4, 2)
+        highres_region = (slice(3, 7),)
+        convert_region_to_low_res(
+            highres_shape=highres_shape,
+            lowres_shape=lowres_shape,
+            highres_region=highres_region,
+        )
+    debug(e.value)
 
     # Incommensurability error (1/2)
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as e:
         highres_shape = (9,)
         lowres_shape = (4,)
         highres_region = (slice(2, 6),)
@@ -105,9 +139,10 @@ def test_convert_region_to_low_res():
             lowres_shape=lowres_shape,
             highres_region=highres_region,
         )
+    debug(e.value)
 
     # Incommensurability error (2/2)
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as e:
         highres_shape = (8,)
         lowres_shape = (4,)
         highres_region = (slice(3, 7),)
@@ -116,3 +151,4 @@ def test_convert_region_to_low_res():
             lowres_shape=lowres_shape,
             highres_region=highres_region,
         )
+    debug(e.value)
