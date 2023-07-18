3655120411010bde91d8fd7cfc55c57c0e3a8481
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-18 14:30:51 +0200

    Improve test_unit_channels.py

--- tests/test_unit_channels.py
+++ tests/test_unit_channels.py
@@ -3,11 +3,74 @@ from pathlib import Path
 
 import jsonschema
 import pytest
+import zarr
 from devtools import debug
 
+from fractal_tasks_core.lib_channels import check_unique_wavelength_ids
+from fractal_tasks_core.lib_channels import check_well_channel_labels
 from fractal_tasks_core.lib_channels import define_omero_channels
 from fractal_tasks_core.lib_channels import get_channel_from_list
 from fractal_tasks_core.lib_channels import OmeroChannel
+
+
+def test_check_unique_wavelength_ids():
+    # Fail for non-unique wavelength_id attributes
+    with pytest.raises(ValueError) as e:
+        check_unique_wavelength_ids(
+            [
+                OmeroChannel(wavelength_id="a"),
+                OmeroChannel(wavelength_id="a"),
+            ]
+        )
+    debug(e.value)
+
+
+def test_check_well_channel_labels(tmp_path):
+    well_group_path = str(tmp_path / "well.zarr")
+    debug(well_group_path)
+    well_group = zarr.group(well_group_path)
+    well_group.attrs.put(
+        dict(
+            well=dict(
+                images=[
+                    dict(path="0"),
+                    dict(path="1"),
+                ]
+            )
+        )
+    )
+    debug(well_group.attrs.asdict())
+    image0_group = well_group.create_group("0")
+    image1_group = well_group.create_group("1")
+    image0_group.attrs.put(
+        dict(
+            omero=dict(
+                channels=[
+                    OmeroChannel(
+                        wavelength_id="id_1", label="non_unique_label"
+                    ).dict(),
+                ]
+            )
+        )
+    )
+    image1_group.attrs.put(
+        dict(
+            omero=dict(
+                channels=[
+                    OmeroChannel(
+                        wavelength_id="id_1", label="non_unique_label"
+                    ).dict(),
+                ]
+            )
+        )
+    )
+    debug(image0_group.attrs)
+    debug(image1_group.attrs)
+
+    with pytest.raises(ValueError) as e:
+        check_well_channel_labels(well_zarr_path=well_group_path)
+    debug(e.value)
+    assert "Non-unique" in str(e.value)
 
 
 def test_get_channel_from_list(testdata_path: Path):
@@ -43,8 +106,19 @@ def test_get_channel_from_list(testdata_path: Path):
     assert channel.wavelength_id == "wavelength_id_2"
     assert channel.index == 1
     # Extract a channel from a list / case 4
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as e:
         channel = get_channel_from_list(channels=omero_channels)
+    debug(e.value)
+
+    # Extract a channel from an inconsistent list
+    channels = [
+        OmeroChannel(wavelength_id="id", label="label"),
+        OmeroChannel(wavelength_id="id", label="label"),
+    ]
+    with pytest.raises(ValueError) as e:
+        get_channel_from_list(channels=channels, label="label")
+    debug(e.value)
+    assert "Inconsistent" in str(e.value)
 
 
 @pytest.fixture(scope="session")
@@ -94,3 +168,16 @@ def test_define_omero_channels(testdata_path: Path, omero_channel_schema):
             "end",
         }
         jsonschema.validate(instance=channel_dict, schema=omero_channel_schema)
+
+    # Fail, due to non-unique labels
+    omero_channels = [
+        OmeroChannel(wavelength_id="id", label="label"),
+        OmeroChannel(wavelength_id="id", label="label"),
+    ]
+    debug(omero_channels)
+    with pytest.raises(ValueError) as e:
+        define_omero_channels(
+            channels=omero_channels, bit_depth=16, label_prefix="prefix"
+        )
+    debug(e.value)
+    assert "Non-unique" in str(e.value)
