9f5a7881eee5944584e9826f33d0750b8e9a0137
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-18 14:50:27 +0200

    Move _inspect_ROI_table to independent module, in `examples/tools`

--- fractal_tasks_core/lib_regions_of_interest.py
+++ fractal_tasks_core/lib_regions_of_interest.py
@@ -272,48 +272,6 @@ def convert_ROI_table_to_indices(
     return list_indices
 
 
-def _inspect_ROI_table(
-    path: str,
-    full_res_pxl_sizes_zyx: Sequence[float],
-    level: int = 0,
-    coarsening_xy: int = 2,
-) -> None:
-    """
-    Description
-
-    :param dummy: this is just a placeholder
-    :type dummy: int
-    """
-
-    print(f"{full_res_pxl_sizes_zyx=}")
-
-    adata = ad.read_zarr(path)
-    df = adata.to_df()
-    print("table")
-    print(df)
-    print()
-
-    try:
-        list_indices = convert_ROI_table_to_indices(
-            adata,
-            level=level,
-            coarsening_xy=coarsening_xy,
-            full_res_pxl_sizes_zyx=full_res_pxl_sizes_zyx,
-            # verbose=True,
-        )
-        print()
-        print(f"level:         {level}")
-        print(f"coarsening_xy: {coarsening_xy}")
-        print("list_indices:")
-        for indices in list_indices:
-            print(indices)
-        print()
-    except KeyError as e:
-        print("Something went wrong in convert_ROI_table_to_indices\n", str(e))
-
-    return df
-
-
 def array_to_bounding_box_table(
     mask_array: np.ndarray, pxl_sizes_zyx: list[float]
 ) -> pd.DataFrame:
