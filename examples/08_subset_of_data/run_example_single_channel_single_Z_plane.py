b279cf89d8d1fcd564498b36256e08199498656f
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-20 11:11:32 +0200

    Hard-code `allowed_channels` in all `examples` scripts, rather than using `tmp_channels.py`

--- examples/08_subset_of_data/run_example_single_channel_single_Z_plane.py
+++ examples/08_subset_of_data/run_example_single_channel_single_Z_plane.py
@@ -1,14 +1,36 @@
 import os
 
 from devtools import debug
-from tmp_channels import allowed_channels
 
+from fractal_tasks_core.lib_channels import OmeroChannel
+from fractal_tasks_core.lib_channels import Window
 from fractal_tasks_core.tasks.copy_ome_zarr import copy_ome_zarr
 from fractal_tasks_core.tasks.create_ome_zarr import create_ome_zarr
 from fractal_tasks_core.tasks.maximum_intensity_projection import (
     maximum_intensity_projection,
 )
 from fractal_tasks_core.tasks.yokogawa_to_ome_zarr import yokogawa_to_ome_zarr
+
+allowed_channels = [
+    OmeroChannel(
+        label="DAPI",
+        wavelength_id="A01_C01",
+        color="00FFFF",
+        window=Window(start=0, end=700),
+    ),
+    OmeroChannel(
+        wavelength_id="A01_C02",
+        label="nanog",
+        color="FF00FF",
+        window=Window(start=0, end=180),
+    ),
+    OmeroChannel(
+        wavelength_id="A02_C03",
+        label="Lamin B1",
+        color="FFFF00",
+        window=Window(start=0, end=1500),
+    ),
+]
 
 
 num_levels = 6
