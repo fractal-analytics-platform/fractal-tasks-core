fd0f178df309136efebd2b3d6772760d3e222e51
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-24 10:34:21 +0200

    Increase timeout in test_workflows_cellpose_segmentation.py from 10 s to 15 s
    This is to avoid frequent CI failures, especially on macos (see e.g. https://github.com/fractal-analytics-platform/fractal-tasks-core/actions/runs/5642471620/job/15282397565#logs)

--- tests/tasks/test_workflows_cellpose_segmentation.py
+++ tests/tasks/test_workflows_cellpose_segmentation.py
@@ -612,7 +612,7 @@ def test_workflow_with_per_FOV_labeling_via_script(
         use_gpu=False,
     )
 
-    run_options = dict(timeout=10, capture_output=True, encoding="utf-8")
+    run_options = dict(timeout=15, capture_output=True, encoding="utf-8")
 
     # Valid model_type -> should fail due to timeout
     this_task_args = dict(**task_args, model_type="nuclei")
