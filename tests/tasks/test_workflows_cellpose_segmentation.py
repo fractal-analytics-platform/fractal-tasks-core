5c6688c3dd038fe63710575b06c369e52c05435e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-08-30 09:55:07 +0200

    Test overwrite-related behavior in cellpose task (ref #458)

--- tests/tasks/test_workflows_cellpose_segmentation.py
+++ tests/tasks/test_workflows_cellpose_segmentation.py
@@ -34,6 +34,7 @@ from ._validation import validate_axes_and_coordinateTransformations
 from ._validation import validate_schema
 from .lib_empty_ROI_table import _add_empty_ROI_table
 from fractal_tasks_core.lib_input_models import Channel
+from fractal_tasks_core.lib_zarr import OverwriteNotAllowedError
 from fractal_tasks_core.tasks.cellpose_segmentation import (
     cellpose_segmentation,
 )
@@ -509,6 +510,37 @@ def test_workflow_bounding_box(
             diameter_level0=80.0,
             output_ROI_table="bbox_table",
         )
+
+    # Re-run with overwrite=True
+    for component in metadata["image"]:
+        cellpose_segmentation(
+            input_paths=[str(zarr_path)],
+            output_path=str(zarr_path),
+            metadata=metadata,
+            component=component,
+            channel=Channel(wavelength_id="A01_C01"),
+            level=3,
+            relabeling=True,
+            diameter_level0=80.0,
+            output_ROI_table="bbox_table",
+            overwrite=True,
+        )
+
+    # Re-run with overwrite=False
+    with pytest.raises(OverwriteNotAllowedError):
+        for component in metadata["image"]:
+            cellpose_segmentation(
+                input_paths=[str(zarr_path)],
+                output_path=str(zarr_path),
+                metadata=metadata,
+                component=component,
+                channel=Channel(wavelength_id="A01_C01"),
+                level=3,
+                relabeling=True,
+                diameter_level0=80.0,
+                output_ROI_table="bbox_table",
+                overwrite=False,
+            )
 
     bbox_ROIs = ad.read_zarr(
         str(zarr_path / metadata["image"][0] / "tables/bbox_table/")
