043e63d7bb4cca421a0d978d6328c53e0b9208e8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-09-01 09:30:19 +0200

    Merge `test_apply_registration.py` into `test_unit_registration_helper_functions.py`

--- tests/tasks/test_unit_registration_helper_functions.py
+++ tests/tasks/test_unit_registration_helper_functions.py
@@ -1,10 +1,17 @@
 import anndata as ad
 import numpy as np
+import pandas as pd
 import pytest
 from devtools import debug
 
 from fractal_tasks_core.tasks.apply_registration_to_ROI_tables import (
     add_zero_translation_columns,
+)
+from fractal_tasks_core.tasks.apply_registration_to_ROI_tables import (
+    apply_registration_to_single_ROI_table,
+)
+from fractal_tasks_core.tasks.apply_registration_to_ROI_tables import (
+    calculate_min_max_across_dfs,
 )
 from fractal_tasks_core.tasks.apply_registration_to_ROI_tables import (
     get_acquisition_paths,
@@ -159,3 +166,108 @@ def test_add_zero_translation_columns():
         add_zero_translation_columns(adata)
     debug(e.value)
     assert "roi table already contains translation columns" in str(e.value)
+
+
+@pytest.fixture
+def translation_table():
+    # Dummy table with calculated shifts per ROI (for 2 ROIs)
+    return [
+        pd.DataFrame(
+            {
+                "translation_z": [0, 0],
+                "translation_y": [4, 5],
+                "translation_x": [9, 11],
+            },
+            index=["FOV_1", "FOV_2"],
+        ),
+        pd.DataFrame(
+            {
+                "translation_z": [0, 0],
+                "translation_y": [5, 6],
+                "translation_x": [-5, 7],
+            },
+            index=["FOV_1", "FOV_2"],
+        ),
+        pd.DataFrame(
+            {
+                "translation_z": [0, 0],
+                "translation_y": [6, -3],
+                "translation_x": [-1, 12],
+            },
+            index=["FOV_1", "FOV_2"],
+        ),
+    ]
+
+
+@pytest.fixture
+def max_df_exp():
+    return pd.DataFrame(
+        {
+            "translation_z": [0, 0],
+            "translation_y": [6, 6],
+            "translation_x": [9, 12],
+        },
+        index=["FOV_1", "FOV_2"],
+    )
+
+
+@pytest.fixture
+def min_df_exp():
+    return pd.DataFrame(
+        {
+            "translation_z": [0, 0],
+            "translation_y": [4, -3],
+            "translation_x": [-5, 7],
+        },
+        index=["FOV_1", "FOV_2"],
+    )
+
+
+@pytest.fixture
+def roi_table():
+    return ad.AnnData(
+        pd.DataFrame(
+            {
+                "x_micrometer": [-1439.300049, -1020.300049],
+                "y_micrometer": [-1517.699951, -1517.699951],
+                "z_micrometer": [0.0, 0.0],
+                "len_x_micrometer": [416.0, 416.0],
+                "len_y_micrometer": [351.0, 351.0],
+                "len_z_micrometer": [0.0, 0.0],
+            },
+            index=["FOV_1", "FOV_2"],
+        )
+    )
+
+
+translated_ROI_table_df = pd.DataFrame(
+    {
+        "x_micrometer": [-1430.300049, -1008.300049],
+        "y_micrometer": [-1511.699951, -1511.699951],
+        "z_micrometer": [0.0, 0.0],
+        "len_x_micrometer": [402.0, 411.0],
+        "len_y_micrometer": [349.0, 342.0],
+        "len_z_micrometer": [0.0, 0.0],
+        "translation_z": [0.0, 0.0],
+        "translation_y": [0.0, 0.0],
+        "translation_x": [0.0, 0.0],
+    },
+    index=["FOV_1", "FOV_2"],
+)
+
+
+def test_calculate_min_max_across_dfs(
+    translation_table, max_df_exp, min_df_exp
+):
+    max_df, min_df = calculate_min_max_across_dfs(translation_table)
+    assert (max_df == max_df_exp).all().all()
+    assert (min_df == min_df_exp).all().all()
+
+
+def test_apply_registration_to_single_ROI_table(roi_table, translation_table):
+    adata_table = add_zero_translation_columns(roi_table)
+    max_df, min_df = calculate_min_max_across_dfs(translation_table)
+    registered_table = apply_registration_to_single_ROI_table(
+        adata_table, max_df, min_df, rois=["FOV_1", "FOV_2"]
+    ).to_df()
+    assert (registered_table == translated_ROI_table_df).all().all()
