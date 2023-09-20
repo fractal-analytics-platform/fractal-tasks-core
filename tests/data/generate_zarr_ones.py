445e904ac9e7a28e42a1c3c4a155b40831a1c8ea
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-09-20 14:40:21 +0200

    Update testing data to better comply with ngff specs

--- tests/data/generate_zarr_ones.py
+++ tests/data/generate_zarr_ones.py
@@ -80,10 +80,14 @@ zattrs = {
             {
                 "wavelength_id": "A01_C01",
                 "label": "some-label-1",
+                "window": {"min": "0", "max": "10"},
+                "color": "00FFFF",
             },
             {
                 "wavelength_id": "A01_C02",
                 "label": "some-label-2",
+                "window": {"min": "0", "max": "10", "start": "0", "end": "10"},
+                "color": "00FFFF",
             },
         ]
     },
