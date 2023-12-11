17aa85d061bf47da111bbb7f5507f960400227e9
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-12-11 12:38:12 +0100

    Remove `lib` prefix from `lib_masked_loading.py

--- fractal_tasks_core/tasks/cellpose_segmentation.py
+++ fractal_tasks_core/tasks/cellpose_segmentation.py
@@ -35,11 +35,11 @@ from fractal_tasks_core.channels import ChannelNotFoundError
 from fractal_tasks_core.channels import get_channel_from_image_zarr
 from fractal_tasks_core.channels import OmeroChannel
 from fractal_tasks_core.input_models import Channel
-from fractal_tasks_core.lib_masked_loading import masked_loading_wrapper
 from fractal_tasks_core.lib_ngff import load_NgffImageMeta
 from fractal_tasks_core.lib_pyramid_creation import build_pyramid
 from fractal_tasks_core.lib_write import prepare_label_group
 from fractal_tasks_core.lib_zattrs_utils import rescale_datasets
+from fractal_tasks_core.masked_loading import masked_loading_wrapper
 from fractal_tasks_core.roi import (
     array_to_bounding_box_table,
 )
