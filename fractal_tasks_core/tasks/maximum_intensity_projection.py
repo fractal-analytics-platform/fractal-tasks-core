04c72880c3a87acca1b78304373b76eb6cfa031f
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-12-11 12:41:34 +0100

    Remove `lib` prefix from `lib_pyramid_creation.py`

--- fractal_tasks_core/tasks/maximum_intensity_projection.py
+++ fractal_tasks_core/tasks/maximum_intensity_projection.py
@@ -22,7 +22,7 @@ from pydantic.decorator import validate_arguments
 from zarr.errors import ContainsArrayError
 
 from fractal_tasks_core.lib_ngff import load_NgffImageMeta
-from fractal_tasks_core.lib_pyramid_creation import build_pyramid
+from fractal_tasks_core.pyramid_creation import build_pyramid
 from fractal_tasks_core.write import OverwriteNotAllowedError
 
 logger = logging.getLogger(__name__)
--- fractal_tasks_core/tasks/napari_workflows_wrapper.py
+++ fractal_tasks_core/tasks/napari_workflows_wrapper.py
@@ -31,8 +31,8 @@ from fractal_tasks_core.channels import get_channel_from_image_zarr
 from fractal_tasks_core.input_models import NapariWorkflowsInput
 from fractal_tasks_core.input_models import NapariWorkflowsOutput
 from fractal_tasks_core.lib_ngff import load_NgffImageMeta
-from fractal_tasks_core.lib_pyramid_creation import build_pyramid
 from fractal_tasks_core.lib_upscale_array import upscale_array
+from fractal_tasks_core.pyramid_creation import build_pyramid
 from fractal_tasks_core.roi import check_valid_ROI_indices
 from fractal_tasks_core.roi import (
     convert_ROI_table_to_indices,
