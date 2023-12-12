4be214613c67a62a26fd85e965491319e1888f28
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-12-12 13:03:48 +0100

    Clarify imports and remove `import *`

--- fractal_tasks_core/ngff/__init__.py
+++ fractal_tasks_core/ngff/__init__.py
@@ -2,7 +2,19 @@
 Subpackage encoding OME-NGFF specifications 0.4 and providing Zarr-related
 tools.
 """
-from .specs import *  # noqa
+from .specs import Axis  # noqa
+from .specs import Channel  # noqa
+from .specs import Dataset  # noqa
+from .specs import ImageInWell  # noqa
+from .specs import Multiscale  # noqa
+from .specs import NgffImageMeta  # noqa
+from .specs import NgffWellMeta  # noqa
+from .specs import Omero  # noqa
+from .specs import ScaleCoordinateTransformation  # noqa
+from .specs import TranslationCoordinateTransformation  # noqa
+from .specs import Well  # noqa
+from .specs import Window  # noqa
+from .zarr_utils import detect_ome_ngff_type  # noqa
 from .zarr_utils import load_NgffImageMeta  # noqa
 from .zarr_utils import load_NgffWellMeta  # noqa
 from .zarr_utils import ZarrGroupNotFoundError  # noqa
