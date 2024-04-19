40ba0e80daa010fbe871b9c7f56b2cf1a9103c41
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-19 12:10:04 +0200

    Add `test_update_well_metadata` (ref #675)

--- tests/tasks/test_unit_zarr_utils.py
+++ tests/tasks/test_unit_zarr_utils.py
@@ -1,5 +1,7 @@
 import logging
 import shutil
+import time
+from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
 import pytest
@@ -12,6 +14,7 @@ from fractal_tasks_core.tasks._registration_utils import (
     _split_well_path_image_path,
 )
 from fractal_tasks_core.tasks._zarr_utils import _copy_hcs_ome_zarr_metadata
+from fractal_tasks_core.tasks._zarr_utils import _update_well_metadata
 
 
 @pytest.mark.parametrize("trailing_slash", [True, False])
@@ -51,3 +54,71 @@ def test_copy_hcs_ome_zarr_metadata(
     debug(well_meta)
     well_paths = [image.path for image in well_meta.well.images]
     assert well_paths == ["0", "0" + suffix]
+
+
+def _star_update_well_metadata(args):
+    """
+    This is only needed because concurrent.futures executors have a `map`
+    method but not a `starmap` one.
+    """
+    return _update_well_metadata(*args)
+
+
+def test_update_well_metadata(
+    tmp_path: Path,
+    testdata_path: Path,
+    caplog: LogCaptureFixture,
+    monkeypatch,
+):
+    """
+    Run _update_well_metadata in parallel for adding N>1 new images to a given
+    well. We artificially slow down each call by INTERVAL seconds, and verify
+    that the test takes at least N x INTERVAL seconds (since each call to
+    `_update_well_metadata` is blocking).
+    """
+
+    N = 4
+    INTERVAL = 0.5
+
+    # Copy a reference zarr into a temporary folder
+    raw_zarrurl = (testdata_path / "plate_ones.zarr").as_posix()
+    zarr_url = (tmp_path / "plate.zarr").resolve().as_posix()
+    shutil.copytree(raw_zarrurl, zarr_url)
+
+    # Artificially slow down `_update_well_metadata`
+    import fractal_tasks_core.tasks._zarr_utils
+
+    def _slow_load_NgffWellMeta(*args, **kwargs):
+        time.sleep(INTERVAL)
+        return load_NgffWellMeta(*args, **kwargs)
+
+    monkeypatch.setattr(
+        fractal_tasks_core.tasks._zarr_utils,
+        "load_NgffWellMeta",
+        _slow_load_NgffWellMeta,
+    )
+
+    # Prepare parallel-execution argument list
+    well_url = Path(zarr_url, "B/03").as_posix()
+    list_args = [(well_url, "0", f"0_new_{suffix}") for suffix in range(N)]
+
+    # Run `_update_well_metadata` N times
+    time_start = time.perf_counter()
+    executor = ProcessPoolExecutor()
+    res_iter = executor.map(_star_update_well_metadata, list_args)
+    list(res_iter)  # This is needed, to wait for all results.
+    time_end = time.perf_counter()
+
+    # Check that time was at least N*INTERVAL seconds
+    assert (time_end - time_start) > N * INTERVAL
+
+    # Check that all new images were added
+    well_meta = load_NgffWellMeta(well_url)
+    well_image_paths = [img.path for img in well_meta.well.images]
+    assert set(well_image_paths) == {
+        "0",
+        "0_new_0",
+        "0_new_1",
+        "0_new_2",
+        "0_new_3",
+    }
