20b55e417e76ae21eeab8d69518495eb6e13e141
jluethi <joel.luethi@uzh.ch>
2024-04-26 18:29:33 +0200

    Add unit tests for cellvoyager wells functions

--- tests/test_unit_cellvoyager_well_names.py
+++ tests/test_unit_cellvoyager_well_names.py
@@ -13,6 +13,7 @@ Zurich.
 """
 import pytest
 
+from fractal_tasks_core.cellvoyager.wells import generate_row_col_split
 from fractal_tasks_core.cellvoyager.wells import get_filename_well_id
 
 params_well_id = [
@@ -26,3 +27,43 @@ params_well_id = [
 @pytest.mark.parametrize("row,col,expected", params_well_id)
 def test_get_filename_well_id(row, col, expected):
     assert get_filename_well_id(row, col) == expected
+
+
+def test_not_implemented_filename_well_id():
+    with pytest.raises(NotImplementedError):
+        get_filename_well_id(row="AAC", col="01234")
+
+
+params_row_col_split = [
+    (
+        ["B03", "B05", "B07", "A01", "H08"],
+        [("A", "01"), ("B", "03"), ("B", "05"), ("B", "07"), ("H", "08")],
+    ),
+    (
+        ["B03.d1", "B05.b1", "B07.c4", "B07.c3", "B07.a4", "A01.a3", "H08.d2"],
+        # Sorting goes by new row name first, then column
+        [
+            ("Aa", "013"),
+            ("Ba", "074"),
+            ("Bb", "051"),
+            ("Bc", "073"),
+            ("Bc", "074"),
+            ("Bd", "031"),
+            ("Hd", "082"),
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize("wells,expected_result", params_row_col_split)
+def test_generate_row_col_split(wells, expected_result):
+    assert generate_row_col_split(wells) == expected_result
+
+
+def test_not_implemented_row_col_split():
+    wells = ["A011"]
+    with pytest.raises(NotImplementedError):
+        generate_row_col_split(wells)
+    wells = ["A01.a01"]
+    with pytest.raises(NotImplementedError):
+        generate_row_col_split(wells)
