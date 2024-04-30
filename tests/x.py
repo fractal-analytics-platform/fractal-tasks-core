0b206e8088aad9af6ebfade4a84a21aa23c73d0e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-30 11:13:15 +0200

    Remove spurious script

--- tests/x.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from fractal_tasks_core.cellvoyager.wells import generate_row_col_split
-
-start = time.perf_counter()
-well_ids = ["B03.d1" for _ in range(1536)]
-generate_row_col_split(well_ids)
-end = time.perf_counter()
-print(f"Elapsed time: {end - start} s")
