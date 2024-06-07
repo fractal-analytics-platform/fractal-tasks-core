53a899b218b0caa5a914fa15cd0689aadeb5f36a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-06-07 08:53:08 +0200

    Improve `test_enum_argument`

--- tests/dev/test_enum_arguments.py
+++ tests/dev/test_enum_arguments.py
@@ -7,54 +7,38 @@ from pydantic.decorator import validate_arguments
 from fractal_tasks_core.dev.lib_args_schemas import (
     create_schema_for_single_task,
 )
-from fractal_tasks_core.tasks.io_models import InitArgsRegistration
 
 
-class Color(Enum):
-    RED = 1
-    GREEN = 2
+class ColorA(Enum):
+    RED = "this-is-red"
+    GREEN = "this-is-green"
 
 
-# @validate_arguments
-def task_function_1(
-    arg1: InitArgsRegistration,
-):
-    """
-    Short task description
-
-    Args:
-        arg1: Description of arg1.
-    """
-    pass
+ColorB = Enum(
+    "ColorB",
+    {"RED": "this-is-red", "GREEN": "this-is-green"},
+    type=str,
+)
 
 
 @validate_arguments
-def task_function_2(
-    arg2: Color,
+def task_function(
+    arg_A: ColorA,
+    arg_B: ColorB,
 ):
     """
     Short task description
 
     Args:
-        arg2: Description of arg2.
+        arg_A: Description of arg_A.
+        arg_B: Description of arg_B.
     """
     pass
-
-
-def test_pydantic_argument():
-    schema = create_schema_for_single_task(
-        task_function=task_function_1,
-        executable=__file__,
-        package=None,
-        verbose=True,
-    )
-    debug(schema)
-    print(json.dumps(schema, indent=2))
 
 
 def test_enum_argument():
     schema = create_schema_for_single_task(
-        task_function=task_function_2,
+        task_function=task_function,
         executable=__file__,
         package=None,
         verbose=True,
