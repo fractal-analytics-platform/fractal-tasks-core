3650305617a83367f4c1359d23279b591bbc981d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-06-07 10:06:15 +0200

    Improve test_unit_lib_descriptions

--- tests/dev/test_unit_lib_descriptions.py
+++ tests/dev/test_unit_lib_descriptions.py
@@ -10,9 +10,9 @@ from fractal_tasks_core.dev.lib_descriptions import (
 
 def test_get_function_args_descriptions():
     args_descriptions = _get_function_args_descriptions(
-        "fractal_tasks_core",
-        "dev/lib_signature_constraints.py",
-        "_extract_function",
+        package_name="fractal_tasks_core",
+        module_relative_path="dev/lib_signature_constraints.py",
+        function_name="_extract_function",
     )
     debug(args_descriptions)
     assert args_descriptions.keys() == set(
@@ -22,7 +22,9 @@ def test_get_function_args_descriptions():
 
 def test_get_class_attrs_descriptions():
     attrs_descriptions = _get_class_attrs_descriptions(
-        "fractal_tasks_core", "channels.py", "ChannelInputModel"
+        package_name="fractal_tasks_core",
+        module_relative_path="channels.py",
+        class_name="ChannelInputModel",
     )
     debug(attrs_descriptions)
     assert attrs_descriptions.keys() == set(("wavelength_id", "label"))
