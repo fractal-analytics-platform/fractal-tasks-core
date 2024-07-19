f3628e7674efa66d23534de5e8b488500f02039f
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-07-19 11:45:52 +0200

    Add assertions to test_otional_argument

--- tests/dev/test_optional_argument.py
+++ tests/dev/test_optional_argument.py
@@ -10,8 +10,9 @@ from fractal_tasks_core.dev.lib_args_schemas import (
 
 @validate_call
 def task_function(
-    arg1: Optional[str] = None,
-    arg2: Optional[list[str]] = None,
+    arg1: str,
+    arg2: Optional[str] = None,
+    arg3: Optional[list[str]] = None,
 ):
     """
     Short task description
@@ -19,12 +20,18 @@ def task_function(
     Args:
         arg1: This is the argument description
         arg2: This is the argument description
+        arg3: This is the argument description
     """
     pass
 
 
 def test_optional_argument():
-
+    """
+    As a first implementation of the Pydantic V2 schema generation, we are not
+    supporting the `anyOf` pattern for nullable attributes. This test verifies
+    that the type of nullable properties is not `anyOf`, and that they are not
+    required.
+    """
     schema = create_schema_for_single_task(
         task_function=validate_call(task_function),
         executable=__file__,
@@ -33,3 +40,7 @@ def test_optional_argument():
     )
     print(json.dumps(schema, indent=2, sort_keys=True))
     print()
+    assert schema["properties"]["arg2"]["type"] == "string"
+    assert "arg2" not in schema["required"]
+    assert schema["properties"]["arg3"]["type"] == "array"
+    assert "arg3" not in schema["required"]
